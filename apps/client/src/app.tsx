699c7983285a390fc2fc1c8a565fd8a496d34388
Moonchichiii <matgus1983@gmail.com>
2026-02-05 11:55:35 +0100

    feat: restructure application by adding main.tsx and app.tsx, and update dependencies

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import '@/';
 const queryClient = new QueryClient();
 
 export default function App() {
